1c7cd8b36fff17f444412ea6f47291855c14c6b7
Sergey Divachuk <sergeydivachuk@gmail.com>
2022-07-18 19:59:03 -0400

    added user routes and controllers

---
+++ controllers/userControllers.js
@@ -0,0 +1,41 @@
+const { User, Thought } = require('models');
+
+module.exports = {
+    //Get all users
+    getUsers(req, res) {
+        User.find()
+          .then((users) => res.json(users))
+          .catch((err) => res.status(500).json(err));
+      },
+
+      //Get single user by userId
+      getSingleUser(req, res) {
+        User.findOne({ _id: req.params.userId })
+          .select('-__v')
+          .then((user) =>
+            !user
+              ? res.status(404).json({ message: 'No user with that ID' })
+              : res.json(user)
+          )
+          .catch((err) => res.status(500).json(err));
+      },
+
+      //Create new user
+      createUser(req, res) {
+        User.create(req.body)
+          .then((user) => res.json(user))
+          .catch((err) => res.status(500).json(err));
+      },
+
+      //Delete user by userId
+      deleteUser(req, res) {
+        User.findOneAndDelete({ _id: req.params.userId })
+          .then((user) =>
+            !user
+              ? res.status(404).json({ message: 'No user with that ID' })
+              : Application.deleteMany({ _id: { $in: user.applications } })
+          )
+          .then(() => res.json({ message: 'User deleted!' }))
+          .catch((err) => res.status(500).json(err));
+      },
+}
